b43d5de6602410f8ffff766247b560140126bfda
Parsa <parsaddshi2002@gmail.com>
2023-01-28 21:16:14 +0330

    add item service

---
+++ CMS/Services/Interfaces/IItemService.cs
@@ -0,0 +1,11 @@
+﻿using CMS.Models;
+
+namespace CMS.Services.Interfaces
+{
+    public interface IItemService
+    {
+        List<Item> GetItems(int count = 9);
+        List<Item> GetFavouriteItems(int count = 6);
+        List<Category> GetCategories();
+    }
+}
---
+++ CMS/Services/ItemService.cs
@@ -0,0 +1,18 @@
+﻿using CMS.Context;
+using CMS.Models;
+using CMS.Services.Interfaces;
+
+namespace CMS.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly CMSContext _context;
+        public ItemService(CMSContext context) => _context = context;
+
+        public List<Category> GetCategories() => _context.Categories.ToList();
+
+        public List<Item> GetFavouriteItems(int count = 6) => _context.Items.OrderByDescending(i => i.Views).Take(count).ToList();
+
+        public List<Item> GetItems(int count = 9) => _context.Items.OrderBy(i => i.CreateDate).Take(count).ToList();      
+    }
+}
