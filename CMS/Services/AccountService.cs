70c3cd49c31ba6d6ddb7859286b8a8ebaf3886ac
Parsa <parsaddshi2002@gmail.com>
2023-01-30 22:07:32 +0330

    add account service and register methods

---
+++ CMS/Services/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using CMS.Context;
+using CMS.Models;
+using CMS.Services.Interfaces;
+
+namespace CMS.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly CMSContext _context;
+        public AccountService(CMSContext context) => _context = context;
+
+        public void InsertUser(User user) => _context.Users.Add(user);
+
+        public bool IsExistByEmail(string email) => _context.Users.Any(u => u.Email == email);
+
+        public bool IsExistByUserName(string userName) => _context.Users.Any(u =>u.UserName == userName);
+
+        public void Save() => _context.SaveChanges();
+    }
+}
