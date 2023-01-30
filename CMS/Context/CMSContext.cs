5d0f57411fd28e182f71651b975d6f11957162cf
Parsa <parsaddshi2002@gmail.com>
2023-01-30 22:06:19 +0330

    add user model to context

--- CMS/Context/CMSContext.cs
+++ CMS/Context/CMSContext.cs
@@ -11,6 +11,7 @@ namespace CMS.Context
         public DbSet<Item> Items { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<CategoryToItem> CategoriesToItems { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
