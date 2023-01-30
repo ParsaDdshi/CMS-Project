a9ebf0f58dbf2b465d045b60ed71134c7180fb30
Parsa <parsaddshi2002@gmail.com>
2023-01-30 22:09:09 +0330

    add register methods to account controller

---
+++ CMS/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using CMS.Models;
+using CMS.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService) => _accountService = accountService;
+
+        #region Register
+        public IActionResult Register() => View();
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if(!ModelState.IsValid) 
+                return View(registerViewModel);
+
+            if (_accountService.IsExistByEmail(registerViewModel.Email.ToLower()))
+            {
+                ModelState.AddModelError("Email", "The entered email is duplicate.");
+                return View(registerViewModel);
+            }
+            if (_accountService.IsExistByUserName(registerViewModel.UserName.ToLower()))
+            {
+                ModelState.AddModelError("UserName", "The entered user name is duplicate.");
+                return View(registerViewModel);
+            }
+
+            User user = new User()
+            {
+                Email = registerViewModel.Email.ToLower(),
+                UserName = registerViewModel.UserName.ToLower(),
+                Password= registerViewModel.Password,
+                RegisterDate= DateTime.Now
+            };
+
+            _accountService.InsertUser(user);
+            _accountService.Save();
+
+            return View("SuccessRegister", user);
+        }
+        #endregion
+    }
+}
