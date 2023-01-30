d82583172fa3ea485acd32d10fe79caaa6935f47
Parsa <parsaddshi2002@gmail.com>
2023-01-30 21:54:28 +0330

    add user model

---
+++ CMS/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime RegisterDate { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
