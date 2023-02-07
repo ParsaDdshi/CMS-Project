b38c05a262b00be1075c18fbb87a8091d61d1e4c
Parsa <parsaddshi2002@gmail.com>
2023-02-07 20:37:13 +0330

    add login view model

---
+++ CMS/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models
+{
+    public class LoginViewModel
+    {
+        [MaxLength(200)]
+        [Required]
+        public string UserNameOrEmail { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
