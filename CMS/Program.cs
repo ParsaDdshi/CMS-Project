0cba3b4c2594c29c38ae9dcf7d8dce45c8313d54
Parsa <parsaddshi2002@gmail.com>
2023-02-07 20:38:49 +0330

    add authentication service to program.cs

--- CMS/Program.cs
+++ CMS/Program.cs
@@ -1,6 +1,7 @@
 using CMS.Context;
 using CMS.Services;
 using CMS.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,14 @@ builder.Services.AddDbContext<CMSContext>(options =>
 
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+        {
+            option.LoginPath = "/Account/Login";
+            option.LogoutPath = "/Account/Logout";
+            option.ExpireTimeSpan = TimeSpan.FromDays(15);
+        });
 
 var app = builder.Build();
 
