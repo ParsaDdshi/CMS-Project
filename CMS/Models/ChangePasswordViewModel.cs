7f5954dbbf1a7c8244e7ae49d7e47d5ba55c28da
Parsa <parsaddshi2002@gmail.com>
2023-02-09 18:06:57 +0330

    add change password view model

---
+++ CMS/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models
+{
+    public class ChangePasswordViewModel
+    {
+
+        [Required]
+        [MaxLength(200)]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword")]
+        public string ReNewPassword { get; set; }
+    }
+}
