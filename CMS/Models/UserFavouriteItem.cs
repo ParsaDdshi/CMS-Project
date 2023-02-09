cf974dc5693dd1d131c06ca103c88cbc4dcac065
Parsa <parsaddshi2002@gmail.com>
2023-02-09 23:48:21 +0330

    add user favourite item model and its relations

---
+++ CMS/Models/UserFavouriteItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Models
+{
+    public class UserFavouriteItem
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        //Navigation Properties
+        public User User { get; set; }
+        public Item  Item{ get; set; }
+    }
+}
