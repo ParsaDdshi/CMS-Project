53ddc55b3df237e9a33631c254c45010df4acfcd
Parsa <parsaddshi2002@gmail.com>
2023-02-14 19:54:24 +0330

    add admin service

---
+++ CMS/Services/Interfaces/IAdminService.cs
@@ -0,0 +1,19 @@
+﻿using CMS.Models;
+
+namespace CMS.Services.Interfaces
+{
+    public interface IAdminService
+    {
+        List<Item> GetAllItems();
+        List<Item> GetCategoryItems(int categoryId);
+        List<Category> GetAllCategories();
+        void InsertItem(Item item);
+        void InsertCategoryToItem(CategoryToItem categoryToItem);
+        AdminItemViewModel GetItemViewModelById(int itemId);
+        Item GetItemById(int itemId);
+        List<int> GetItemCategoryId(int itemId);
+        void DeleteCategoryToItems(int itemId);
+        void RemoveItem(int itemId);
+        void Save();
+    }
+}
