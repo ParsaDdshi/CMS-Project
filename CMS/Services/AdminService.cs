0a3d8f6398b03001800de8362dc018fbc15881d7
Parsa <parsaddshi2002@gmail.com>
2023-02-18 21:23:02 +0330

    add user admin section methods to service

--- CMS/Services/AdminService.cs
+++ CMS/Services/AdminService.cs
@@ -9,11 +9,15 @@ namespace CMS.Services
     {
         private readonly CMSContext _context;
         private readonly IItemService _itemService;
-        public AdminService(CMSContext context, IItemService itemService) 
+        private readonly IAccountService _accountService;
+        public AdminService(CMSContext context, IItemService itemService, IAccountService accountService) 
         {
             _context = context;
-            _itemService= itemService;
+            _itemService = itemService;
+            _accountService = accountService;
         }
+
+        #region Item Admin Section
 
         public List<Category> GetAllCategories() => _itemService.GetCategories();
 
@@ -38,8 +42,6 @@ namespace CMS.Services
 
         public void InsertItem(Item item) => _context.Items.Add(item);
 
-        public void Save() => _context.SaveChanges();
-
         public Item GetItemById(int itemId) => _itemService.GetItemById(itemId);
 
         public void DeleteCategoryToItems(int itemId) => _context.CategoriesToItems
@@ -50,5 +52,30 @@ namespace CMS.Services
             Item item = GetItemById(itemId);
             _context.Items.Remove(item);
         }
+
+        #endregion
+
+        #region User Admin Section
+
+        public List<User> GetAllUsers() => _context.Users.ToList();
+
+        public void AddUser(User user) => _context.Users.Add(user);
+
+        public User GetUserById(int userId) => _accountService.GetUserById(userId);
+
+        public bool IsUserExist(int userId) => _context.Users.Any(u => u.UserId == userId);
+
+        public void RemoveUser(User user) => _context.Users.Remove(user);
+
+        public void RemoveUserFavItems(int userId) => _context.UserFavouriteItems
+            .Where(u => u.UserId == userId).ToList().ForEach(i => _context.UserFavouriteItems.Remove(i));
+
+        public List<User> GetUsersByRole(bool isAdmin) => isAdmin == true ? 
+            _context.Users.Where(u => u.IsAdmin == isAdmin).ToList() : 
+            _context.Users.Where(u => u.IsAdmin == isAdmin).ToList();
+
+        #endregion
+
+        public void Save() => _context.SaveChanges();
     }
 }
--- CMS/Services/Interfaces/IAdminService.cs
+++ CMS/Services/Interfaces/IAdminService.cs
@@ -4,6 +4,8 @@ namespace CMS.Services.Interfaces
 {
     public interface IAdminService
     {
+
+        #region Items Admin Section
         List<Item> GetAllItems();
         List<Item> GetCategoryItems(int categoryId);
         List<Category> GetAllCategories();
@@ -14,6 +16,20 @@ namespace CMS.Services.Interfaces
         List<int> GetItemCategoryId(int itemId);
         void DeleteCategoryToItems(int itemId);
         void RemoveItem(int itemId);
+
+        #endregion
+
+        #region User Admin Section
+
+        List<User> GetAllUsers();
+        void AddUser(User user);
+        User GetUserById(int userId);
+        bool IsUserExist(int userId);
+        void RemoveUser(User user);
+        void RemoveUserFavItems(int userId);
+        List<User> GetUsersByRole(bool isAdmin);
+
+        #endregion 
         void Save();
     }
 }
