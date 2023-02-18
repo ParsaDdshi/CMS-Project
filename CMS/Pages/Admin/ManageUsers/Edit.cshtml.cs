8075f6ba3d87a8848731e4dad29b1cfd935b8f9a
Parsa <parsaddshi2002@gmail.com>
2023-02-18 21:27:16 +0330

    add manage users edit razor page

---
+++ CMS/Pages/Admin/ManageUsers/Edit.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CMS.Context;
+using CMS.Models;
+using CMS.Services.Interfaces;
+
+namespace CMS.Pages.Admin.ManageUsers
+{
+    public class EditModel : PageModel
+    {
+        private readonly IAdminService _adminService;
+
+        public EditModel(IAdminService adminService) => _adminService = adminService;
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            User = _adminService.GetUserById(id.Value);
+            if (User == null)
+                return NotFound();
+            
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            User user = _adminService.GetUserById(User.UserId);
+            user.UserName = User.UserName;
+            user.Email = User.Email;
+            user.Password = User.Password;
+
+            try
+            {
+              _adminService.Save();
+            }      
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_adminService.IsUserExist(User.UserId))
+                    return NotFound();
+                else
+                    throw;   
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
